11f210f0c232a6dcb432d97e4ccb175c037f771e
Tissiana Alves <altissiana@gmail.com>
2020-07-08 14:06:56 -0600

    implemented parallax effect

--- DesignCodeApp/ViewController.swift
+++ DesignCodeApp/ViewController.swift
@@ -46,5 +46,14 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UIScrollViewDelegate{
-    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        if offsetY < 0 {
+            heroView.transform = CGAffineTransform(translationX: 0, y: offsetY)
+            playVisualView.transform = CGAffineTransform(translationX: 0, y: -offsetY/3)
+            titleLabel.transform = CGAffineTransform(translationX: 0, y: -offsetY/3)
+            deviceImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY/4)
+            backgroundImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY/5)
+        }
+    }
 }
