afbf1596703744b3a189a2d3592dc520ff3842c3
Tissiana Alves <altissiana@gmail.com>
2020-07-08 12:11:34 -0600

    created a SectionViewController class to display the video screen

---
+++ DesignCodeApp/SectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SectionViewController.swift
+//  DesignCodeApp
+//
+//  Created by Tissiana Alves on 7/8/20.
+//  Copyright © 2020 Tissiana Alves. All rights reserved.
+//
+
+import UIKit
+
+class SectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
