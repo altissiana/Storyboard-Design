26cd18eaf6662d24ef1ea3961a4a36aff0107b16
Tissiana Alves <altissiana@gmail.com>
2020-07-07 18:25:37 -0600

    initializing repo

---
+++ DesignCodeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  test.swift
+//  DesignCodeApp
+//
+//  Created by Tissiana Alves on 7/7/20.
+//  Copyright © 2020 Tissiana Alves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
